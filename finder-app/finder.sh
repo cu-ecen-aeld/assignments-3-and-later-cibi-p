94a909a644bc4dbff7f6a025ed95bf2d3955739e
cibi.p <brohari740@gmail.com>
2026-01-03 14:47:03 +0530

    Completed the finder assignment

---
+++ finder-app/finder.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+  echo "Requires two arguments"
+  exit 1
+fi
+
+FILEDIR=$1
+SEARCHSTR=$2
+
+if [ ! -d $FILEDIR ]; then
+  echo "$1, Directory not found"
+  exit 1
+fi
+
+Totalfiles=$(find $FILEDIR -type f | wc -l)
+MatchedLines=$(grep -r $SEARCHSTR $FILEDIR | wc -l)
+
+echo "The number of files are $Totalfiles and the number of matching lines are $MatchedLines"
