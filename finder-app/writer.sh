5253e1c4e2375e43875b3afd2ae0fffdb0a2c455
cibi.p <brohari740@gmail.com>
2026-01-03 14:47:23 +0530

    completed the writer.sh assignment

---
+++ finder-app/writer.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+  echo "Requires two arguments"
+  exit 1
+fi
+
+FILEPATH=$1
+WRITESTRING=$2
+
+DIRPATH=$(dirname $FILEPATH)
+if [ ! -d $DIRPATH ]; then
+  mkdir $DIRPATH
+fi
+
+echo $WRITESTRING >$FILEPATH
