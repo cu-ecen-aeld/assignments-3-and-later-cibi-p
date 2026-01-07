da62487f4235c7f90c85b3d15287a608101e84a5
cibi.p <brohari740@gmail.com>
2026-01-07 09:12:28 +0530

    completed writer.c part

---
+++ finder-app/writer.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <syslog.h>
+
+#define ERROR   1
+#define SUCCESS 0
+
+int main(int argc, char** argv) {
+  char *filePath    = argv[1];
+  char *writeString = argv[2];
+  int ret           = SUCCESS;
+  FILE *fptr        = NULL;
+
+  openlog(NULL, "", LOG_USER);
+
+  if (argc < 3) {
+    syslog(LOG_ERR, "Requires two arguments");
+    ret = ERROR;
+    goto exit;
+  }
+
+  fptr = fopen(filePath, "w");
+  fprintf(fptr, "%s\n", writeString);
+  syslog(LOG_DEBUG, "Writing %s to %s\n", filePath, writeString);
+
+exit:
+  return ret;
+}
