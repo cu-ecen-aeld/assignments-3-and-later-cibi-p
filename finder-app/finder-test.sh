90e9cdaf2d79e20771ba478022b601cfb95070d9
cibi.p <brohari740@gmail.com>
2026-01-11 00:17:06 +0530

    removed the make command from this finder-test.sh

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -10,8 +10,6 @@ WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
 username=$(cat conf/username.txt)
 
-make
-
 if [ $# -lt 3 ]; then
   echo "Using default value ${WRITESTR} for string to write"
   if [ $# -lt 1 ]; then
