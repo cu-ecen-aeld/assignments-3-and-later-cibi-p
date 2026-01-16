af6bb89a32d6f8a38e1699d880154b4d34c12f9b
cibi.p <brohari740@gmail.com>
2026-01-16 14:13:37 +0530

    examples/threading: completed the threading task

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -14,6 +14,12 @@ void* threadfunc(void* thread_param)
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
     //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    usleep(thread_func_args->wait_to_obtain_ms * 1000);
+    pthread_mutex_lock(thread_func_args->lock);
+    usleep(thread_func_args->wait_to_release_ms * 1000);
+    pthread_mutex_unlock(thread_func_args->lock);
+    thread_func_args->thread_complete_success = true;
     return thread_param;
 }
 
@@ -28,6 +34,16 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+    struct thread_data *threadData = (struct thread_data *) malloc(sizeof(struct thread_data));
+    bool ret = true;
+    threadData->thread = thread;
+    threadData->lock = mutex;
+    threadData->wait_to_obtain_ms = wait_to_obtain_ms;
+    threadData->wait_to_release_ms = wait_to_release_ms;
+    if (pthread_create(thread, NULL, threadfunc, threadData) != 0) {
+        ERROR_LOG("pthread_create failed");
+        ret = false;
+    }
+    return ret;
 }
 
